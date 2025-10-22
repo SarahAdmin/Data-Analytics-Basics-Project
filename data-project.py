9b1d9afbf4ec14402456369350fceb8484fb4cf0
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-10-22 21:22:01 +0100

    Change CSV file name from 'data.csv' to 'ticket_data.csv'

--- data-project.py
+++ data-project.py
@@ -1,3 +1,3 @@
 import pandas as pd 
-df = pd.read_csv('data.csv',encoding='latin-1') // To read CSV file, create the const called 'df' then insert a csv file by importing the file// 
-print(df) // Print whole data //
+df = pd.read_csv('ticket_data.csv',encoding='latin-1') /* To read CSV file, create the const called 'df' then insert a csv file by importing the file */ 
+print(df) /* Print whole data */
